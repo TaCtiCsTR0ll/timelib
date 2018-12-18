199a753c10472974945ba4f09f525ddae610c533
Sebastian <sebastiangreger98@googlemail.com>
2018-12-18 10:13:23 +0100

    Added isLeapYear function to determine if a year is a leap year

--- main.c
+++ main.c
@@ -18,6 +18,49 @@ int day_of_the_year(int day, int month, int year)
     return 0;
 }
 
+/**
+Die Funktion ermittelt für ein übergebenes Jahr des gregorianischen
+Kalenders, ob es sich um ein Schaltjahr handelt.
+
+year: Das zu überprüfende Jahr
+
+returns: '1', wenn Schaltjahr
+         '0', wenn kein Schaltjahr
+         '-1', wenn das Jahr vor dem Beginn des gregorianischen Kalenders liegt
+**/
+int isLeapyear(int year)
+{
+    if(year >= 1582)
+    {
+        if(year%4 == 0)
+        {
+            if(year%100 == 0)
+            {
+                if(year%400 == 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    else
+    {
+        return -1;
+    }
+}
+
 int main()
 {
     printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2018));
